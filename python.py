d0d22866c1fbb36cdf511d7225e47b0ce26521e5
Setu94275 <setupatel012@gmail.com>
2025-06-24 23:07:13 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -9,7 +9,7 @@ users = {
 }
 
 def main():
-    
+    st.button("login now")
     
     
     st.sidebar.header("User Login")
@@ -74,11 +74,27 @@ st.write(f"Selected : {pesticide}")
 st.button("conform now")
 
 
+
+
 import streamlit as st
 col1,col2,col3= st.columns(3)
 with col1 :
     st.header("PENDI")
     st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFRXQN6zeIjNntgBqXgVqeP74Aq5agpfp0pQ&s", width=200)
+    
+    
+    col8,col9,col10 = st.columns(3)
+with col8 :
+     with st.expander("PENDI PESISIDE INSTRUCTIONS") :
+      st.write("""1.PENDI (30%EC)
+
+ USES(વપરાશ )    (નિંદામણ નાશક)
+
+(1) મગફરી , ઘઉં  , કપાસ ,સોયાબીન ,તુવેર  .
+
+(2) વાવેતર પછી 2 દિવસ અંદર  છટકાવ  માટે  જમીન માં ભેજ હોય તે જરરી છે .
+
+(4) એક પંપ (15 લીટર ) માં 80ml થી 100ml  """)   
 
 with col2 :
     st.header("REJENT")
@@ -87,7 +103,10 @@ with col3 :
     st.header("BLACK AMRUT")
     st.image("https://agribegri.com/productimage/18963318401729577015.webp",width=200)
 
+
+
 col8,col9,col10 = st.columns(3)
+
 with col8 :
     with st.expander("PENDI PESISIDE INSTRUCTIONS") :
      st.write("""1.PENDI (30%EC)
@@ -98,7 +117,10 @@ with col8 :
 
 (2) વાવેતર પછી 2 દિવસ અંદર  છટકાવ  માટે  જમીન માં ભેજ હોય તે જરરી છે .
 
-(4) એક પંપ (15 લીટર ) માં 80ml થી 100ml  """)    
+(4) એક પંપ (15 લીટર ) માં 80ml થી 100ml  """)   
+
+
+
      
 with col9  :
  with st.expander("REJENT PESISIDE INSTRUCTIONS") :    
@@ -106,7 +128,9 @@ with col9  :
 Early shoot borer, stem borer, leaf folder, and termites in rice
 Green leafhopper and other significant pests in sugarcane and wheat.
 Fipronil works primarily through ingestion and contact, causing toxicity by disrupting the insect's nervous system """)
-    
+
+
+
     with col10 :
         with st.expander("BLACK AMRUT PESISIDE INSTRUCTION") :
             st.write("""It is a mixture of essential nutrients, soluble carbon and natural growth enhancers, made from Organic and Natural Extracts, which makes it safe and eco-friendly. It improves the production of crops via acting on all stages like Plant Growth, Fruiting, Flowering & Size Improvement.""")
