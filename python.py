e5f78f65be8054dbcae110afba1b4912ce559e80
Setu94275 <setupatel012@gmail.com>
2025-06-25 23:27:35 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -9,8 +9,7 @@ users = {
 }
 
 def main():
-    
-
+    st.button("login now")
     
     
     st.sidebar.header("User Login")
@@ -34,17 +33,35 @@ if __name__ == "__main__":
 st.title ("MUKUND AGRO SEEDS")
 
 st.subheader("RANASAN")
-if st.button("complet your profile"):
-    st.text_input("ENTER NAME HARE:")
-    st.text_input("VILLAGE NAME") 
-    st.text_input("TALUKA") 
-    st.text_input("DISTRICT")
-    st.text_input("STATE")
-    st.selectbox("(HOW MANY YEARS TO CLOSE FARMING :", ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"])
-    st.text_input("WHERE YOU BUY PESTICIDE")
-    button = st.button("SAVE")
-    st.success=(" TAHNKS FOR SHARE YOUR DITAILS")
+col4,col5 = st.columns(2)
+with col4 :
+    st.text_input("ENTER NAME HARE: ")
 
+with col5 :
+    st.text_input("VILLAGE NAME")    
+
+col6,col7 =st.columns(2)
+
+
+with col6 :
+    st.text_input("TALUKA")
+
+with col7:
+    st.text_input("DISTRICT")
+
+with col6 :
+    st.text_input("STATE")    
+
+
+
+classdeta = st.selectbox("HOW MANY YEARS TO CLOSE FARMING :", ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"])
+
+
+bname = st.text_input("where are you buy pestiside ")
+
+button = st.button("SAVE")
+
+st.success("Thank you for join mukund agro seeds")
    
  
 st.write("pleas select pesicide")
@@ -57,49 +74,15 @@ st.write(f"Selected : {pesticide}")
 st.button("conform now")
 
 
-
-
 import streamlit as st
 col1,col2,col3= st.columns(3)
 with col1 :
     st.header("PENDI")
     st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFRXQN6zeIjNntgBqXgVqeP74Aq5agpfp0pQ&s", width=200)
-    
-col8,col9,col10 =st.columns(3)    
-    
-with col8 :
-     with st.expander("PENDI PESISIDE INSTRUCTIONS") :
-      st.write("""1.PENDI (30%EC)
-
- USES(વપરાશ )    (નિંદામણ નાશક)
-
-(1) મગફરી , ઘઉં  , કપાસ ,સોયાબીન ,તુવેર  .
-
-(2) વાવેતર પછી 2 દિવસ અંદર  છટકાવ  માટે  જમીન માં ભેજ હોય તે જરરી છે .
-
-(4) એક પંપ (15 લીટર ) માં 80ml થી 100ml  """)   
 
 with col2 :
     st.header("REJENT")
     st.image("https://kissanemart.com/storage/bayer-regent-sc-insecticide-800x800.jpg",width=250)
-
-with col9  :
- with st.expander("REJENT PESISIDE INSTRUCTIONS") :    
-    st.write("""Regent insecticides contain fipronil, a phenylpyrazole compound known for its efficacy in controlling various difficult-to-kill pests, including:
-Early shoot borer, stem borer, leaf folder, and termites in rice
-Green leafhopper and other significant pests in sugarcane and wheat.
-Fipronil works primarily through ingestion and contact, causing toxicity by disrupting the insect's nervous system """)
-
 with col3 :
     st.header("BLACK AMRUT")
     st.image("https://agribegri.com/productimage/18963318401729577015.webp",width=200)
-    with col10 :
-        with st.expander("BLACK AMRUT PESISIDE INSTRUCTION") :
-            st.write("""It is a mixture of essential nutrients, soluble carbon and natural growth enhancers, made from Organic and Natural Extracts, which makes it safe and eco-friendly. It improves the production of crops via acting on all stages like Plant Growth, Fruiting, Flowering & Size Improvement.""")
-
-            
-            
-if st.button("click now"):
-   url = "https://www.blogger.com/blog/post/edit/preview/8120710627423734129/8353132355958769146"
-   st.text_input("completed")
-   st.image("https://kissanemart.com/storage/bayer-regent-sc-insecticide-.jpg",width=500)
